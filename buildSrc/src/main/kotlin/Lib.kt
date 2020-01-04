90846fad001a8bf6559c52638371a7c767b4acce
ShabanKamel <sh3ban.kamel@gmail.com>
2020-01-04 12:02:21 +0400

    add classic & compose modules

--- buildSrc/src/main/kotlin/Lib.kt
+++ buildSrc/src/main/kotlin/Lib.kt
@@ -1,4 +1,6 @@
 object Lib {
-    const val core = ":core"
-    const val rxJava = ":rxjava"
+    const val compose = ":lib:compose"
+    const val classic = ":lib:classic:classic"
+    const val rxJava = ":lib:classic:rxjava"
+    const val core = ":lib:core"
 }
