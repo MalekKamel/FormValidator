3b04ec2a3d921d218f7cf4e7bf57799cc3d2dcf1
Shaban <sh3ban.kamel@gmail.com>
2021-05-30 03:53:05 +0200

    Upgrade Gradle

---
+++ lib/compoz/build/generated/source/buildConfig/release/com/sha/compoz/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sha.compoz;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.sha.compoz";
+  public static final String BUILD_TYPE = "release";
+}
